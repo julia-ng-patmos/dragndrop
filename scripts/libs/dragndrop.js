5d25351f37d9a37dd31df213335e45bb99b5da7e
Julian <julian.eonlt@gmail.com>
2014-10-11 10:54:53 -0500

    Antiguo drag n drop duplicando los elementos (clone node)

--- scripts/libs/dragndrop.js
+++ scripts/libs/dragndrop.js
@@ -1,105 +1,127 @@
 /**
  * Created by eon-1 on 9/12/14.
  */
-
-function isMobile() {
-    return (
-        (navigator.userAgent.match(/Android/i)) ||
-        (navigator.userAgent.match(/webOS/i)) ||
-        (navigator.userAgent.match(/iPhone/i)) ||
-        (navigator.userAgent.match(/iPod/i)) ||
-        (navigator.userAgent.match(/iPad/i)) ||
-        (navigator.userAgent.match(/BlackBerry/))
-        );
-}
-
-var app = angular.module('dragDrop', ['angular-gestures']);
+var app = angular.module('dragDrop', []);
 
 app.controller("DragDropCtrl", function ($scope) {
     $scope.handleDrop = function () {
         alert("Item has been dropped");
-    };
+    }
 });
 
-app.directive('draggable', ['$document', function ($document) {
-    return {
-        controller: function ($scope) {
-            $scope.handleGesture = function ($event) {
-//                console.log($event);
-                $scope.type = $event.type;
-            }
-        },
-        link: function (scope, element, attr) {
-            var startX = 0, startY = 0, x = 0, y = 0;
-            console.log(attr);
-            if (isMobile()) {
+app.directive('draggable', function () {
+    return function (scope, element) {
+        var el = element[0];
+        var crt;
+        var trick;
 
-            }
+        el.draggable = true;
 
-            element.bind('hola');
-            console.log(element)
+        el.addEventListener(
+            'dragstart',
+            function (e) {
+                /*********************************************/
+                trick = document.createElement("div");
+                trick.id = "coverup";
+                trick.style.background = "white";
+                trick.style.position = "absolute";
+                trick.style.top = "0px";
+                trick.style.right = "0px";
+                trick.style.width = "6%";
+                trick.style.height = "6%";
+                trick.style.zIndex = "2";
+                /**********************************************/
+                document.body.appendChild(trick);
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData('Text', this.id);
+                crt = this.cloneNode(true);
+                this.style.opacity = "0";
+                crt.style.position = "absolute";
+                crt.style.top = "0px";
+                crt.style.right = "0px";
+                document.body.appendChild(crt);
+                e.dataTransfer.setDragImage(crt, 0, 0);
 
-            if (!element.context.classList.contains('imagehover')) {
-                element.css({
-                    position: 'relative',
-                    cursor: 'pointer'
-                });
-            }
+                return false;
+            },
+            false
+        );
 
-            element.on('mousedown', function (event) {
-                // Prevent default dragging of selected content
-                event.preventDefault();
-                startX = event.pageX - x;
-                startY = event.pageY - y;
-                $document.on('mousemove', mousemove);
-                $document.on('mouseup', mouseup);
-            });
-
-            function mousemove(event) {
-                y = event.pageY - startY;
-                x = event.pageX - startX;
-                element.css({
-                    top: y + 'px',
-                    left: x + 'px'
-                });
-            }
-
-            var myVar;
-
-            function retroceso() {
-                if (x < 0) {
-                    x++;
-                } else if (x > 0) {
-                    x--;
-                }
-
-                if (y < 0) {
-                    y++
-                } else if (y > 0) {
-                    y--
-                }
-
-                element.css({
-                    top: y + 'px',
-                    left: x + 'px'
-                });
-
-                if (x == 0 && y == 0) {
-                    clearTimeout(myVar);
-                }
-            }
-
-            function mouseup() {
-                $document.off('mousemove', mousemove);
-                $document.off('mouseup', mouseup);
-                myVar = setInterval(retroceso, 1);
-            }
-        }
-    };
-}]);
-
-app.directive('droppable', ['$document', function ($document) {
-    return function (scope, element, attr) {
-//        console.log(element)
+        el.addEventListener(
+            "dragend",
+            function (e) {
+                this.style.opacity = "1";
+                document.body.removeChild(crt);
+                document.body.removeChild(trick);
+                return false;
+            },
+            false
+        );
     }
-}]);
+});
+
+app.directive("droppable", function () {
+    return{
+        scope: {
+            drop: "&",
+            bin: "="
+        },
+        replace: true,
+        link: function (scope, element) {
+            // again we need the native object
+            var el = element[0];
+
+            el.addEventListener("dragover", function (e) {
+                    e.dataTransfer.dropEffect = "move";
+                    //allows us to drop
+                    if (e.preventDefault) e.preventDefault();
+                    this.classList.add("over");
+                    return false;
+                },
+                false
+            );
+
+            el.addEventListener(
+                "dragenter",
+                function (e) {
+                    this.classList.add('over');
+                    return false;
+                },
+                false
+            );
+
+            el.addEventListener(
+                'dragleave',
+                function (e) {
+                    this.classList.remove("over");
+                    return false;
+                },
+                false
+            );
+
+            el.addEventListener(
+                "drop",
+                function (e) {
+                    if (e.stopPropagation) e.stopPropagation();
+
+                    this.classList.remove("over");
+
+                    var binId = this.id;
+                    var item = document.getElementById(e.dataTransfer.getData("Text"));
+                    this.appendChild(item);
+
+                    // call the drop passed drop function
+                    scope.$apply(function (scope) {
+                        var fn = scope.drop();
+                        if ("undefined" !== typeof fn) {
+                            fn(item.id, binId);
+                        }
+                    });
+
+                    return false;
+                },
+                false
+            )
+        }
+    }
+});
